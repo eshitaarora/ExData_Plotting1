5c9cb7ceb50d82268bfe7cc96090c4d51004ff59
eshitaarora <78889909+eshitaarora@users.noreply.github.com>
2022-11-06 16:49:35 +0530

    Add files via upload

---
+++ R files/2.TimeSeries.R
@@ -0,0 +1,2 @@
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
